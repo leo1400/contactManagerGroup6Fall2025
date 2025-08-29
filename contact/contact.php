3126fe169e0d3232275b2c798ecc35131d7c61f1
baldlow <theb3stn0scoper555@gmail.com>
2025-08-29 17:15:09 -0400

    added sessions for login api, added js to handle login/logout,updated dockercompose

---
+++ contact/contact.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['userid'])){
+		header("Location: http://localhost:8080/");
+	}
+?>
+<html>
+
+<head>
+	<title>contacty</title>
+	<script src="../js/code.js"></script>
+</head>
+
+<body>
+	<p>Welcome you are signed in</p>
+	<div id="logoutButton">
+			<button onclick="doLogout()">Logout</button>
+	</div>
+
+</body>
+
+</html>
