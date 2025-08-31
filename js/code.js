a9bc8fc6cc24f7d7af79c3adb3f1f65f4fc74b12
baldlow <theb3stn0scoper555@gmail.com>
2025-08-31 13:59:18 -0400

    implemented the add contact function

--- js/code.js
+++ js/code.js
@@ -60,6 +60,31 @@ function doRegistration() {
 	htr.setRequestHeader("Content-Type", "application/json");
 	htr.send(jsonUserInfo);
 }
+function addContact() {
+	let contactFirstName = document.getElementById("newContactFirstName").value;
+	let contactLastName = document.getElementById("newContactLastName").value;
+	let contactPhone = document.getElementById("newContactPhone").value;
+	let contactEmail = document.getElementById("newContactEmail").value;
+
+	// we add the user id in api;
+	let contactInfo = { "firstname": contactFirstName, "lastname": contactLastName, "phone": contactPhone, "email": contactEmail };
+	let jsonContactInfo = JSON.stringify(contactInfo);
+
+	let url = urlBase + "Add_Contact" + phpBase;
+	let htr = new XMLHttpRequest();
+
+	htr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			const result = JSON.parse(htr.responseText);
+			let spanAddContactResult = document.getElementById("addContactResult");
+			spanAddContactResult.innerHTML = result.message;
+		}
+	}
+	htr.open("POST", url, true);
+	htr.setRequestHeader("Content-Type", "application/json");
+	htr.send(jsonContactInfo);
+
+}
 function doLogout() {
 	window.location.href = loginUrlBase + "logout" + phpBase;
 }
