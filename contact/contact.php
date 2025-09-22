129e7b2f09389402c362757cadb2bc593083fc2f
baldlow <theb3stn0scoper555@gmail.com>
2025-09-22 14:18:50 -0400

    fixed pathing issue for code refernce

--- contact/contact.php
+++ contact/contact.php
@@ -9,7 +9,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width" />
 		<title>contacty</title>
-		<script src="js/code.js"></script>
+		<script src="../js/code.js"></script>
 		<link rel="stylesheet" href="css/styles.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	</head>
